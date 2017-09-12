0f218433b46c5a1730cd3d5e5e2e5edcafe62297
Brian Retterer <bretterer@gmail.com>
2017-09-12 19:39:14 -0400

    Adds travis config with a test

---
+++ tests/PlaceholderTest.php
@@ -0,0 +1,13 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class PlaceholderTest extends TestCase
+{
+    /** @test */
+    public function it_works()
+    {
+        $this->assertTrue(true); 
+    }
+    
+}
